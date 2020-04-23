8a75c7084c2974aea4800bc837ed6408f62e8966
Sanjeev Kumar Mishra <sanjeevroy56@gmail.com>
2020-04-24 01:30:39 +0530

    This Code Fetches All Items from a particular list from a SharePoint Site.
    For Executing this code and all the other codes for CSOM Properly You Need to do Few things :
    
    
    1->Create a Sharepoint Site and Add a List to it with Columns as - 'Title' (Type- Text), 'Your_Name'(Type-Text), 'Roll_No'(Number), 'Subject' (Type-Text), 'MyField2(Number)' , and Add some rows to it.
    2-> Create 'Workflow Console Project' on your Visual Studio IDE.
    3->Add 2-reference (.dll) files to the AddReference section - Microsoft.Sharepoint.Client & Microsoft.Sharepoint.Client.RunTime. (you need to download these from Microsoft site first)
    4->Paste above Code in the 'Program.CS' File.
    5->Write your Office365 UserName in the space Provided in the Program.
    6->Write your SharepointSite URL in the space Provided in the Program. (Ex-> https://George.sharepoint.com/sites/Your_siteName )
    7->Execute the Code
    8->Enter your password in the pop-up Console .
                  AND  THERE YOU GO. YOUR OUTPUT IS READY !!!!!!!!!

---
+++ GET_Opearion.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using System.Runtime.Remoting.Contexts;
+using SP = Microsoft.SharePoint.Client;
+using System.Collections.Generic;
+//using Microsoft.Sharepoint.Client.QueryExpression;
+
+namespace AddListData
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Write-Your-Office365-UserName-Within-these-Double-Quotes";
+            Console.WriteLine("Enter your password");           
+            SecureString password = GetPassword();
+             
+            //Client Context Gets Context of SharePoint Online Site     //Site URL
+            using (var clientContext = new ClientContext("Write-Your-Sharepoint-Site-URL-Within-these-Double-Quotes")) 
+            {
+                //Sharepoint Online Credentials
+                clientContext.Credentials = new SharePointOnlineCredentials(userName, password);
+
+      
+                try
+                {
+                     List Fetched_List = clientContext.Web.Lists.GetByTitle("Second_List");
+
+                    // This creates a CamlQuery that has a RowLimit of 100, and also specifies Scope="RecursiveAll"
+                    // so that it grabs all list items, regardless of the folder they are in.
+                    CamlQuery query = CamlQuery.CreateAllItemsQuery(100);
+                    ListItemCollection items = Fetched_List.GetItems(query);
+
+                    // Retrieve all items in the ListItemCollection from List.GetItems(Query).
+                    clientContext.Load(items);
+                    clientContext.ExecuteQuery();
+
+                    Console.WriteLine("Title" + "\t" + "Your_Name" + "\t" + "Id" + "\t" + "Subject" + "\t" + "MyField2");
+                    foreach (ListItem listItem in items)
+                    {
+                        Console.WriteLine(listItem["Title"] + "\t" + listItem["Your_Name"] + "\t" + listItem["Roll_No"] + "\t" + listItem["Subject"] + "\t" + listItem["MyField2"]);
+                    }
+                 }
+                 
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception Occured" + ex.Message);
+                }
+            }
+        }
+
+
+        private static SecureString GetPassword()
+        {
+            ConsoleKeyInfo info;
+            //Get the user's password as a SecureString
+            SecureString securePassword = new SecureString();
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    securePassword.AppendChar(info.KeyChar);
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return securePassword;
+        }
+    }
+}
+
+                 
+                
